f165c0a818b0e30ef7f7b0a726735511c9e1300c
Kevinleyva190 <kevinleyva190@gmail.com>
2023-05-06 21:31:08 -0600

    Actividad extra 3

---
+++ src/Ventana.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+
+public class Ventana extends Frame {
+    private Label lbletiqueta1;
+    private Label lbletiqueta2;
+    private TextField txtcuadro1;
+    private TextField txtcuadro2;
+    private Button boton1;
+    private FlowLayout layout;
+    public Ventana(String title) throws HeadlessException {
+        super(title);
+        layout=new FlowLayout(FlowLayout.LEFT);
+        this.setLayout(layout);
+        lbletiqueta1=new Label("Etiqueta 1");
+        this.add(lbletiqueta1);
+        txtcuadro1=new TextField(60);
+        this.add(txtcuadro1);
+        lbletiqueta2=new Label("Etiqueta 2");
+        this.add(lbletiqueta2);
+        txtcuadro2=new TextField(60);
+        this.add(txtcuadro2);
+        this.setSize(800,600);
+        this.setVisible(true);
+    }
+}
